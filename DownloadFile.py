0bad4871e3c596294c6d6f50c2db2e4f328e3001
Ahmed Hazem <150187393+ahmed-hazem-1@users.noreply.github.com>
2024-09-13 15:49:22 +0300

    Add files via upload
    Fix the issue of File detection

--- DownloadFile.py
+++ DownloadFile.py
@@ -1,12 +1,14 @@
+import mimetypes
 import os
 import urllib.request
 import urllib.error
 import urllib.parse
 import requests
-import time
-import ssl
-import certifi
+# import time
+# import ssl
+# import certifi
 import urllib.request
+import filetype
 
 
 def get_name(url):
@@ -21,9 +23,8 @@ def get_name(url):
 
 def get_info(url):
     try:
-        ssl_context = ssl.create_default_context(cafile=certifi.where())
+        response = requests.get(url, stream=True, verify=False)
 
-        response = urllib.request.urlopen(url, context=ssl_context)
         mime_type = response.headers.get('Content-Type', 'Unknown')
         File_size = response.headers.get('Content-Length', None)
         if File_size is not None:
@@ -33,12 +34,31 @@ def get_info(url):
         else:
             file_size = "Unknown"
 
+        # Read the first 1024 bytes once
+        data = response.raw.read(1024)
+
+        # If the server didn't provide a MIME type, guess based on the file extension
+        if mime_type == 'Unknown':
+            file_extension = os.path.splitext(url)[1]
+            mime_type = mimetypes.types_map.get(file_extension, 'Unknown')
+
+        # If the MIME type is still unknown, use filetype to guess the file type
+        if mime_type == 'Unknown':
+            kind = filetype.guess(data)
+            if kind is None:
+                mime_type = 'Unknown'
+            else:
+                mime_type = kind.mime
+
+        # If the server didn't provide a file size, estimate the size based on the first 1024 bytes
+        if file_size == 'Unknown' and len(data) == 1024:
+            file_size = f"{os.path.getsize(url) / (1024 * 1024):.2f} MB"
+
         return mime_type, file_size
 
-    except urllib.error.URLError as e:
+    except requests.RequestException as e:
         print(f"Error getting file info: {e}")
         return "Unknown", "Unknown"
-
 
 # def download_file(url, file_path, progress, max_retries=3, block_size=1024 * 1024):  # 1 Mebibyte
 #     try:
@@ -81,7 +101,8 @@ def download_file(url, file_path, progress, max_retries=3):  # Removed block_siz
         total_size = int(response.headers.get('Content-Length', 0)) + file_size
 
         # Set block size relative to total size
-        block_size = total_size if total_size < 1024 else max(1024, total_size // 1000)  # At least 1 KB, and allows for 100 updates
+        block_size = total_size if total_size < 1024 else max(1024,
+                                                              total_size // 1000)  # At least 1 KB, and allows for 100 updates
 
         mode = 'ab' if file_size > 0 else 'wb'
         with open(file_path, mode) as file:
